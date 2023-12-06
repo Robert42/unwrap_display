a056c5ae770cffa95614bd07d3f27d665c0d274f
Robert <robert.3dgamedev@gmail.com>
2023-12-06 13:13:14 +0100

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,41 @@
 #![no_std]
 
+/*!
+When an error happens, [`core::result::Result::unwrap`] prints the error using the [`core::fmt::Debug`] instead of the [`core::fmt::Display`] trait.
+That's ok as the result of `unwrap` is not meant to be seen by end users  anyway.
+
+Some error types are also incomprehensible for developers when being formatted using `Debug` instead of `Display`.
+In this case formatting the error with `Display` can increase productivity.
+A typical usecase would be errors from parsers inside unittests.
+
+This crate adds the
+[`unwrap_display`](UnwrapDisplay::unwrap_display)
+and
+[`expect_display_code`](UnwrapDisplay::unwrap_display)
+methods to [`core::result::Result`].
+
+Now you can write `do_something_interesting().unwrap_display()` in your tests and get the error formatted with `Display` instead of `Debug.`
+*/
+
+/** A type implementing this trait provides the `unwrap_display` method 
+*/
 pub trait UnwrapDisplay
 {
+	/** Type of the value to return if there was no error.
+	*/
   type Inner;
 
+	/** Returns the inner value when there was no error.
+
+	When there was an error it panics displaying the error value.
+	*/
   #[track_caller]
   fn unwrap_display(self) -> Self::Inner;
 
+	/** Returns the inner value when there was no error.
+
+	When there was an error it panics displaying the passed code.
+	*/
   #[track_caller]
   fn expect_display_code(self, code: &str) -> Self::Inner;
 }
